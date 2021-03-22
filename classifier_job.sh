4279167d4fcf315a31ac089831425a0f88593cf2
schurgerfoy <schurgerfoy@chapman.edu@bright81.chapman.edu>
2021-03-22 16:22:18 -0700

    try lda and svm on volume data

--- classifier_job.sh
+++ classifier_job.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --time=0-0:00:01
-#SBATCH --job-name=PYTEST
+#SBATCH --time=0-0:10:00
+#SBATCH --job-name=crossVal
 #SBATCH --error=job.%J.err
 #SBATCH --output=job.%J.out
 
-python --version
+module load anaconda3/current
+conda init bash
+conda activate slurmenv
+
+# python3 pca_validation.py
