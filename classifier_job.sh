9b7182339e3f665be6b4666a0cf95847249b149f
Adrien Schurger-Foy <adriensf@gmail.com>
2021-05-24 13:42:30 -0700

    add linear svm loo crosval job

--- classifier_job.sh
+++ classifier_job.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --time=0-0:10:00
-#SBATCH --job-name=crossVal
+#SBATCH --time=0-10:00:00
+#SBATCH --job-name=shaplot
 #SBATCH --error=job.%J.err
 #SBATCH --output=job.%J.out
 
 module load anaconda3/current
+eval "$(conda shell.bash hook)"
 conda init bash
-conda activate slurmenv
+conda activate shapenv
 
-# python3 pca_validation.py
+python3 linear_svm_crossval.py
---
+++ linear_svm_crossval.py
@@ -0,0 +1,96 @@
+import pathlib
+import shap
+import mlflow
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.utils import resample
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from parallel_kfold import ParallelKFold
+
+
+def resample_balanced(volume_filename: str):
+    volumes = pd.read_csv(volume_filename)
+
+    # add other label format
+    volumes["Target"] = volumes["Target"].astype('category')
+    volumes['Target_cat'] = volumes['Target'].cat.codes 
+
+
+    # Subsampling from the dataset 
+    # Creating the Minority
+    volumes['Target'].value_counts()
+    df_minority = volumes[volumes.Target=='AD']
+    df_minority
+
+    #Creating a Majority for CN 
+    df_CN = volumes[volumes.Target=='CN']
+    # Downsample majority class
+    cn_sampled = resample(df_CN, 
+                                    replace=False,    # sample without replacement
+                                    n_samples=76,     # to match minority class
+                                    random_state=123) # reproducible results
+
+
+    #Creating a Majority for MCI 
+    df_MCI = volumes[volumes.Target=='MCI']
+    # Downsample majority class
+    mci_sampled = resample(df_MCI, 
+                                    replace=False,    # sample without replacement
+                                    n_samples=76,     # to match minority class
+                                    random_state=123) # reproducible results
+
+    #Creating a Majority for MCI 
+    df_SPR = volumes[volumes.Target=='SPR']
+    # Downsample majority class
+    spr_sampled = resample(df_SPR, 
+                                    replace=False,    # sample without replacement
+                                    n_samples=76,     # to match minority class
+                                    random_state=123) # reproducible results
+
+    
+
+    # creating a new df with subsampled dfs
+    pdList = [cn_sampled, mci_sampled, spr_sampled]  # List of your dataframes
+    df_majority = pd.concat(pdList)
+    df_majority["Target"].value_counts()
+
+
+    # Combine minority class with downsampled majority class
+    df_downsampled = pd.concat([df_minority, df_majority])
+
+
+    limited = df_downsampled
+    limited = limited.drop(columns=['Target', 'Target_cat'])
+
+
+    return limited, df_downsampled
+
+
+def run_svm(X_train, y_train, X_val, y_val):
+    clf = SVC(kernel='linear', probability=True)
+    clf.fit(X_train, y_train)
+
+    mlflow.log_param('input_features', input_features)
+    return clf.score(X_val, y_val)
+
+
+
+
+limited, full = resample_balanced('volumes.csv')
+
+loocv = ParallelKFold()
+acc = loocv.k_fold(limited.shape[0], run_svm, limited, full['Target_cat'])
+print("linear svm leave one out cross-validation accuracy (balanced unscaled dataset):", acc)
+
+
+
+limited, full = resample_balanced('scaled_volumes.csv')
+
+loocv = ParallelKFold()
+acc = loocv.k_fold(limited.shape[0], run_svm, limited, full['Target_cat'])
+print("linear svm leave one out cross-validation accuracy (balanced unscaled dataset):", acc)
